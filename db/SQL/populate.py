0aea47d86bf13d77a8a49468356c30548a768619
Larko <larionov1001@gmail.com>
2022-07-29 23:13:35 +0300

    Updated standalone scripts populate and recreate

--- db/SQL/populate.py
+++ db/SQL/populate.py
@@ -1,43 +1,6 @@
 import sqlite3
 import psycopg2
-from dotenv import load_dotenv
-import pathlib
-import os
-
-load_dotenv(str(pathlib.Path(__file__).parent.parent.parent.joinpath('.env')))
-
-
-def connect_sqlite3():
-    conn = sqlite3.connect('db/data.db')
-    cursor = conn.cursor()
-    return conn, cursor
-
-
-# def connect_postgres():
-#     conn = psycopg2.connect(
-#         database='pizza_bot_db',
-#         user='pizza_user',
-#         password='pizza'
-#     )
-#     return conn, conn.cursor()
-
-
-def connect_postgres():
-    conn = psycopg2.connect(
-        database='d2vqfbhdetfr8m',
-        user='gtlhzircpzzgnq',
-        password=os.environ['DB_PASSWORD'],
-        host='ec2-54-170-90-26.eu-west-1.compute.amazonaws.com',
-        port=5432
-    )
-    return conn, conn.cursor()
-
-
-def connect(db='postgres'):
-    if db == 'postgres':
-        return connect_postgres()
-    elif db == 'sqlite3':
-        return connect_sqlite3()
+from db.db import connect
 
 
 ingredients = [
@@ -178,26 +141,26 @@ pizzas = [
     }
 ]
 
+if __name__ == '__main__':
+    conn, cursor = connect()
 
-conn, cursor = connect()
-
-for ingr in ingredients:
-    cursor.execute(f"INSERT INTO Ingredient (name, price) VALUES (%s, %s)", list(ingr.values()))
-    conn.commit()
+    for ingr in ingredients:
+        cursor.execute(f"INSERT INTO Ingredient (name, price) VALUES (%s, %s)", list(ingr.values()))
+        conn.commit()
 
 
-for pizza in pizzas:
-    cursor.execute(f"INSERT INTO Pizza (name, is_custom, is_proto, size) VALUES (%s, %s, %s, %s)", (pizza['name'],
-                                                                                                False, True, 1))
-    conn.commit()
+    for pizza in pizzas:
+        cursor.execute(f"INSERT INTO Pizza (name, is_custom, is_proto, size) VALUES (%s, %s, %s, %s)", (pizza['name'],
+                                                                                                    False, True, 1))
+        conn.commit()
 
-    cursor.execute(f"SELECT id FROM Pizza WHERE name=%s ", [pizza['name']])
-    pizza_id = cursor.fetchall()[0][0]
+        cursor.execute(f"SELECT id FROM Pizza WHERE name=%s ", [pizza['name']])
+        pizza_id = cursor.fetchall()[0][0]
 
-    for ingr in pizza['ingredients']:
-        cursor.execute(f"SELECT id FROM Ingredient WHERE name=%s ", [ingr['ingr']])
-        ingr_id = cursor.fetchall()[0][0]
+        for ingr in pizza['ingredients']:
+            cursor.execute(f"SELECT id FROM Ingredient WHERE name=%s ", [ingr['ingr']])
+            ingr_id = cursor.fetchall()[0][0]
 
-        query = f"INSERT INTO IngredientInPizza (ingredient_id, pizza_id, grams) VALUES ({ingr_id}, {pizza_id}, {ingr['grams']})"
-        cursor.execute(query)
-    conn.commit()
+            query = f"INSERT INTO IngredientInPizza (ingredient_id, pizza_id, grams) VALUES ({ingr_id}, {pizza_id}, {ingr['grams']})"
+            cursor.execute(query)
+        conn.commit()
